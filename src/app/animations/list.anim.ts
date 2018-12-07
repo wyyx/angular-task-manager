f1cd5bda25a4770cc46ea2144d537cf9ef588955
wyyx <gnehcyx@163.com>
2018-12-07 20:17:09 +0800

    fix animation bugs

--- src/app/animations/list.anim.ts
+++ src/app/animations/list.anim.ts
@@ -7,7 +7,8 @@ import {
 	animate,
 	group,
 	query,
-	stagger
+	stagger,
+	animateChild
 } from '@angular/animations'
 
 export const listAnim = trigger('list', [
@@ -16,27 +17,27 @@ export const listAnim = trigger('list', [
 			':enter',
 			[
 				style({ transform: 'scale(0)', opacity: 0 }),
-				group([
-					animate('0.5s ease-in-out', style({ transform: 'scale(1)' })),
-					animate('0.5s ease-in-out', style({ opacity: 1 }))
+				stagger(100, [
+					group([
+						animate('0.5s ease-in-out', style({ transform: 'scale(1)' })),
+						animate('0.5s ease-in-out', style({ opacity: 1 }))
+					])
 				])
 			],
 			{ optional: true }
 		),
-		query(':enter', stagger(100, [ animate('1s', style({ opacity: 1 })) ]), {
-			optional: true
-		}),
 		query(
 			':leave',
 			[
 				style({ transform: 'scale(1)', opacity: 1 }),
-				group([
-					animate('0.5s', style({ transform: 'scale(0)' })),
-					animate('0.5s', style({ opacity: 0 }))
+				stagger(100, [
+					group([
+						animate('0.5s', style({ transform: 'scale(0)' })),
+						animate('0.5s', style({ opacity: 0 }))
+					])
 				])
 			],
 			{ optional: true }
-		),
-		query(':leave', stagger(100, [ animate('1s', style({ opacity: 0 })) ]), { optional: true })
+		)
 	])
 ])
