8387973ff7f30771c5207b961f5e31068097039a
wyyx <gnehcyx@163.com>
2019-02-04 12:28:35 +0800

    fixed add or remove self from members of a project

--- src/app/project/invite/invite.component.ts
+++ src/app/project/invite/invite.component.ts
@@ -1,15 +1,16 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core'
 import { FormControl } from '@angular/forms'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
-import { Store } from '@ngrx/store'
+import { Store, select } from '@ngrx/store'
 import { Subject } from 'rxjs'
-import { map, take, takeUntil, tap } from 'rxjs/operators'
+import { map, take, takeUntil, tap, withLatestFrom } from 'rxjs/operators'
 import { User } from 'src/app/auth/models/user.model'
 import { Project } from 'src/app/domain/project.model'
 import { UserService } from 'src/app/services/user.service'
 import { Chip } from 'src/app/shared/models/chip.model'
 import { AppState } from 'src/app/store'
 import { AddOrRemoveMembersAction } from '../store/actions/project.actions'
+import { getUser } from 'src/app/auth/store/selectors/auth.selectors'
 
 @Component({
   selector: 'app-invite',
@@ -38,10 +39,16 @@ export class InviteComponent implements OnInit, OnDestroy {
     this.userService
       .getUsersByProject(this.project)
       .pipe(
-        tap(users => {
+        withLatestFrom(this.store.pipe(select(getUser))),
+        tap(([users, currentUser]) => {
           this.previousMembers = users
+
+          // Set initial chips
           this.members.setValue(
-            this.previousMembers.map(user => ({ label: user.name, value: user }))
+            // Exclude currentUser
+            this.previousMembers
+              .filter(u => u.id !== currentUser.id)
+              .map(user => ({ label: user.name, value: user }))
           )
         }),
         take(1)
@@ -59,13 +66,22 @@ export class InviteComponent implements OnInit, OnDestroy {
 
     console.log('this.currentMembers', this.currentMembers)
 
-    this.store.dispatch(
-      new AddOrRemoveMembersAction({
-        projectId: this.project.id,
-        previousMembers: this.previousMembers,
-        currentMembers: this.currentMembers
-      })
-    )
+    this.store
+      .pipe(
+        select(getUser),
+        tap(currentUser =>
+          this.store.dispatch(
+            new AddOrRemoveMembersAction({
+              projectId: this.project.id,
+              previousMembers: this.previousMembers,
+              // Include currentUser
+              currentMembers: [...this.currentMembers, currentUser]
+            })
+          )
+        ),
+        take(1)
+      )
+      .subscribe()
 
     this.dialog.close()
   }
@@ -79,7 +95,7 @@ export class InviteComponent implements OnInit, OnDestroy {
       .searchUsers(filter)
       .pipe(
         tap(v => console.log('[debug]', 'members', v)),
-        // Exclude exists members and self
+        // Exclude exists members
         map(members => members.filter(m => !this.previousMembers.map(pm => pm.id).includes(m.id))),
         tap(members => (this.searchedChips = members.map(m => ({ label: m.name, value: m })))),
         take(1)
